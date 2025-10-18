178554f94193db817100b99f3cc69dc6b3f6e9f7
LionelXIV <sarrlionel03@gmail.com>
2025-10-17 23:03:31 -0300

    Template base, page accueil initiale

--- coeur/views.py
+++ coeur/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def accueil(request):
+    """Vue pour la page d'accueil"""
+    return render(request, 'coeur/accueil.html')
--- projet_inforoute/settings.py
+++ projet_inforoute/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'projet_inforoute.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
